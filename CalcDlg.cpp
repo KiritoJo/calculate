ca010d78c2031409c208dd1ce4c2b4613a301f5c
KiritoJo <119726966+KiritoJo@users.noreply.github.com>
2022-12-03 15:57:27 +0800

    Add files via upload

---
+++ CalcDlg.cpp
@@ -0,0 +1,258 @@
+﻿
+// CalcDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Calc.h"
+#include "CalcDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// 根据需要添加
+#include <string>
+using namespace std;
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCalcDlg 对话框
+
+
+
+CCalcDlg::CCalcDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CALC_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CCalcDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT1, m_edit1);
+	DDX_Control(pDX, IDC_show_res, m_show_res);
+	DDX_Control(pDX, IDC_show_res, m_show_res);
+	  DDX_Control(pDX, IDC_CopyRight, m_copyright);
+}
+
+BEGIN_MESSAGE_MAP(CCalcDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_EN_CHANGE(IDC_EDIT1, &CCalcDlg::OnEnChangeEdit1)
+	ON_BN_CLICKED(IDC_BUTTON1, &CCalcDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CCalcDlg::OnBnClickedButton2)
+	//ON_EN_CHANGE(IDC_EDIT4, &CCalcDlg::OnEnChangeEdit4)
+END_MESSAGE_MAP()
+
+
+// CCalcDlg 消息处理程序
+
+BOOL CCalcDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	m_edit1.SetWindowTextW(TEXT("请输入一个十进制整数"));
+	//m_static_result.SetWindowTextW(TEXT("结果："));
+	m_copyright.SetWindowTextW(TEXT("188制作"));
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CCalcDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CCalcDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CCalcDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CCalcDlg::OnEnChangeEdit1()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+void ShiftChange(CString &str)
+{
+	long long t = _ttoll(str);
+	string s = "";
+	while (t)
+	{
+		int tt = t%16;
+		if (tt <= 9)
+			s += to_string(tt);
+		else
+			s += (tt - 10) + 'a';
+		t /= 16;
+	}
+	reverse(s.begin(), s.end());
+	str = s.c_str();
+}
+
+bool checkEdit1(CString str)
+{
+	string s = CT2A(str.GetBuffer());
+	for (int i = 0; i < s.size(); i++)
+		if (!('0' <= s[i] && s[i] <= '9'))
+			return false;
+	return true;
+}
+
+void CCalcDlg::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CString str;
+	m_edit1.GetWindowTextW(str);
+	if (str.IsEmpty())
+		MessageBox(TEXT("输入为空！"), TEXT("提示"));
+	else if (!checkEdit1(str))
+		MessageBox(TEXT("仅可输入数字！"), TEXT("提示"));
+	//string sz2 = CT2A(str.GetBuffer());
+	else
+	{
+		ShiftChange(str);
+		m_show_res.SetWindowTextW(str);
+	}
+}
+
+void CCalcDlg::OnOK()
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	//CDialogEx::OnOK();
+}
+
+
+void CCalcDlg::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
+
+
+void CCalcDlg::OnEnChangeEdit4()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+
+
+
+
+void CCalcDlg::OnStnClickedCopyright()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
---
+++ CalcDlg.h
@@ -0,0 +1,48 @@
+﻿
+// CalcDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCalcDlg 对话框
+class CCalcDlg : public CDialogEx
+{
+// 构造
+public:
+	CCalcDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	CEdit m_edit1;
+	afx_msg void OnBnClickedButton1();
+	CEdit m_edit2;
+	afx_msg void OnEnChangeEdit2();
+	CEdit m_result;
+	virtual void OnOK();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnEnChangeEdit4();
+	CEdit m_show_res;
+//	CEdit m_copyright;
+	afx_msg void OnStnClickedCopyright();
+	CEdit m_copyright;
+};
